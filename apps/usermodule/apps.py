38fc420103ff1e085ddafb1b993fb60f54e47503
raihanah7 <raihanahyo@gmail.com>
2026-02-22 11:32:32 +0300

    Lab 3

--- apps/usermodule/apps.py
+++ apps/usermodule/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class UsermoduleConfig(AppConfig):
-    name = 'usermodule'
+    name = 'apps.usermodule'
+    
--- libraryproject/settings.py
+++ libraryproject/settings.py
@@ -9,12 +9,13 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, "apps" + os.sep + "templates")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.bookmodule',
+    'apps.usermodule',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'libraryproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
